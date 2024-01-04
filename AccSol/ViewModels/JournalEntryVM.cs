636d732eced239ee5463510422d743e5db24305a
Solomio S. Sisante <solomiosisante@gmail.com>
2024-01-04 21:00:20 +0800

    EditForm and QuickGrid now working in PettyCash

---
+++ AccSol/ViewModels/JournalEntryVM.cs
@@ -0,0 +1,9 @@
+﻿using AccSol.EF.Models;
+
+namespace AccSol.ViewModels
+{
+    public class JournalEntryVM : JournalEntry
+    {
+        public string? AccountName { get; set; }
+    }
+}
---
+++ AccSol/ViewModels/PettyCashVM.cs
@@ -0,0 +1,10 @@
+﻿using AccSol.EF.Models;
+
+namespace AccSol.ViewModels
+{
+    public class PettyCashVM : PettyCash
+    {
+        public string? ClientName { get; set; }
+        public string? ProjectName { get; set; }
+    }
+}
