16c7f23c106476cf0c38e113f577c804114c7031
Solomio S. Sisante <solomiosisante@gmail.com>
2024-01-07 14:43:29 +0800

    #6 Fixed the filtering, Add, Edit Delete for both PettyCash and Journal Entry.

--- AccSol/ViewModels/JournalVM.cs
+++ AccSol/ViewModels/JournalVM.cs
@@ -1,4 +1,6 @@
-﻿using AccSol.EF.Models;
+﻿using AccSol.Components.Pages.Client;
+using AccSol.Components.Pages.ProjectCode;
+using AccSol.EF.Models;
 using System.ComponentModel.DataAnnotations;
 
 namespace AccSol.ViewModels
@@ -7,12 +9,41 @@ namespace AccSol.ViewModels
     {
         public string? AccountName { get; set; }
         private List<JournalVM> _journalVMEntryList = new List<JournalVM>(); // Initialize the list
+        private List<Coa> _coas = new List<Coa>(); // Initialize the list
         public bool IsEditing { get; set; } // Boolean property to indicate editing mode
 
+        public JournalVM(Journal journal, List<Coa> coas)
+        {
+            _coas = coas;
+
+            ID = journal.ID;
+            PettyCashId = journal.PettyCashId;
+            Debit = journal.Debit;
+            Credit = journal.Credit;
+
+            int coaId = journal.CoaId ?? _coas[0].ID;
+            CoaId = coaId;
+
+            AccountName = GetAccountName(coaId);
+        }
+        public JournalVM(JournalVM journalVM, List<Coa> coas)
+        {
+            _coas = coas;
+
+            ID = journalVM.ID;
+            PettyCashId = journalVM.PettyCashId;
+            Debit = journalVM.Debit;
+            Credit = journalVM.Credit;
+
+            int coaId = journalVM.CoaId ?? _coas[0].ID;
+            CoaId = coaId;
+
+            AccountName = journalVM.CoaId != null? journalVM.AccountName : GetAccountName(coaId);
+            
+        }
         public JournalVM()
         {
         }
-
         public void SetJournalEntryVMList(List<JournalVM> journalVMEntryList)
         {
             _journalVMEntryList = journalVMEntryList;
@@ -28,10 +59,10 @@ namespace AccSol.ViewModels
             }
 
             // Implement your custom validation logic here
-            if (!IsEditing && AlreadyExists(AccountName, ID)) // Check existence only in editing mode
-            {
-                yield return new ValidationResult("AccountName already exists.", new[] { nameof(AccountName) });
-            }
+            //if (!IsEditing && AlreadyExists(AccountName, ID)) // Check existence only in editing mode
+            //{
+            //    yield return new ValidationResult("AccountName already exists.", new[] { nameof(AccountName) });
+            //}
         }
 
         private bool AlreadyExists(string accountName, int currentItemId)
@@ -46,6 +77,16 @@ namespace AccSol.ViewModels
             }
 
             return alreadyExists;
+        }
+
+        private string GetAccountName(int coaId)
+        {
+            string name = string.Empty;
+
+            var foundName = _coas?.FirstOrDefault(c => c.ID == coaId)?.AccountName;
+            name = foundName ?? string.Empty;
+            
+            return name;
         }
     }
 }
